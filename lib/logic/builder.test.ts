ab41144575b15ee69ddce731ad5bd85250c917c6
Vytenis Urbonavičius <vytenisu@wix.com>
2019-05-07 15:07:09 +0300

    Preparation for building component consumer code

--- lib/logic/builder.test.ts
+++ lib/logic/builder.test.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs'
 import * as mkdirp from 'mkdirp'
 import * as path from 'path'
 import * as rimraf from 'rimraf'
@@ -20,29 +21,51 @@ const INJECTED_VARIABLES: IProjectVariableValues = {
   },
 }
 
+const OUTPUT_DIR = path.resolve(__dirname, '../../cache/test')
+
 let builder: Builder
 
 describe('Builder', () => {
-  const OUTPUT_DIR = path.resolve(__dirname, '../../cache/test/')
-  const testPath = path.dirname(OUTPUT_DIR)
-
-  beforeEach(() => {
-    mkdirp.sync(testPath)
-
-    builder = new Builder(
-      MOCK_COMPONENT_STRUCTURE,
-      path.resolve(__dirname, '../templates/consumers'),
-      path.resolve(__dirname, '../../cache/builder'),
-    )
-  })
-
-  afterEach(() => {
-    rimraf.sync(testPath)
+  beforeAll(() => {
+    builder = new Builder(MOCK_COMPONENT_STRUCTURE, path.resolve(__dirname, '../../cache/builder'))
   })
 
   describe('generate', () => {
-    it('generates sources for each component', () => {
+    beforeAll(() => {
       builder.generate(OUTPUT_DIR, INJECTED_VARIABLES)
+    })
+
+    afterAll(() => {
+      rimraf.sync(OUTPUT_DIR)
+    })
+
+    it('generates sources for each component', () => {
+      expect(fs.readdirSync(OUTPUT_DIR)).toEqual(Object.keys(MOCK_COMPONENT_STRUCTURE))
+    })
+
+    it('injects component name into component source code', () => {
+      const componentNames = Object.keys(MOCK_COMPONENT_STRUCTURE)
+      const componentName = componentNames[componentNames.length - 1]
+      const componentPath = path.resolve(OUTPUT_DIR, componentName, `${componentName}.ts`)
+      const source = fs.readFileSync(componentPath, {encoding: 'utf8'})
+      expect(source.indexOf(componentName)).not.toBe(-1)
+    })
+  })
+
+  describe('build', () => {
+    beforeAll(() => {
+      builder.build(OUTPUT_DIR, INJECTED_VARIABLES)
+    })
+
+    afterAll(() => {
+      rimraf.sync(OUTPUT_DIR)
+    })
+
+    it('builds separate styles for each component', () => {
+      // TODO: Not finished
+    })
+
+    it('builds JS for each component', () => {
       // TODO: Not finished
     })
   })
--- lib/logic/builder.ts
+++ lib/logic/builder.ts
@@ -2,6 +2,7 @@ import * as ejs from 'ejs'
 import * as fs from 'fs'
 import * as mkdirp from 'mkdirp'
 import * as path from 'path'
+import rimraf = require('rimraf')
 import {IComponentStructure} from '../interfaces/shared'
 import {FileSystem} from './file-system'
 
@@ -10,6 +11,40 @@ import {FileSystem} from './file-system'
  * It is replaced to actual component name when interpolated
  */
 export const COMPONENT_NAME_PLACEHOLDER = '$component'
+
+/**
+ * Location of templates
+ */
+export const TEMPLATE_PATH = path.resolve(__dirname, '../templates')
+
+/**
+ * Path to template for generate component consumers
+ */
+export const CONSUMER_TEMPLATE_PATH = path.resolve(TEMPLATE_PATH, 'consumers')
+
+/**
+ * Path to consumer webpack configuration template
+ */
+export const CONSUMER_WEBPACK_CONFIG_TEMPLATE_PATH = path.resolve(
+  TEMPLATE_PATH,
+  'webpack',
+  'consumer.webpack.config.js.ejs',
+)
+
+/**
+ * Location of temporary generated code when building
+ */
+export const GENERATED_CODE_TMP_DIR = 'generated'
+
+/**
+ * Location of generated webpack configuration
+ */
+export const GENERATED_WEBPACK_CONFIG_PATH = 'webpack/webpack.config.js'
+
+/**
+ * Temporary location for built code
+ */
+export const BUILT_CODE_TMP_DIR = 'built'
 
 /**
  * Builds/generates components with or without injected variable values
@@ -27,11 +62,11 @@ export class Builder {
    * @param templatePath path where component template is placed
    * @param tmpPath path where to store temporary files during operations
    */
-  constructor(componentStructure: IComponentStructure, templatePath: string, tmpPath: string) {
+  constructor(componentStructure: IComponentStructure, tmpPath: string) {
     this.componentStructure = componentStructure
     this.tmpPath = tmpPath
     this.prepareTmpPath()
-    this.loadTemplate(templatePath)
+    this.loadTemplate(CONSUMER_TEMPLATE_PATH)
   }
 
   /**
@@ -40,6 +75,10 @@ export class Builder {
    * @param injectedVariableValues variable values to be injected
    */
   public build(outputPath: string, injectedVariableValues: IProjectVariableValues = {}) {
+    const generatedCodePath = path.resolve(this.tmpPath, GENERATED_CODE_TMP_DIR)
+    rimraf.sync(generatedCodePath)
+    this.generate(generatedCodePath, injectedVariableValues)
+    this.generateConsumerWebpackConfig(outputPath)
     // TODO: Not finished
   }
 
@@ -56,6 +95,14 @@ export class Builder {
         injectedVariableValues[componentName] || {},
       )
     })
+  }
+
+  private generateConsumerWebpackConfig(outputPath: string) {
+    const template = fs.readFileSync(CONSUMER_WEBPACK_CONFIG_TEMPLATE_PATH, {encoding: 'utf8'})
+    const code = ejs.render(template, {input: path.resolve(this.tmpPath, GENERATED_CODE_TMP_DIR), output: outputPath})
+    const generatedConfigPath = path.resolve(this.tmpPath, GENERATED_WEBPACK_CONFIG_PATH)
+    mkdirp.sync(path.dirname(generatedConfigPath))
+    fs.writeFileSync(generatedConfigPath, code, {encoding: 'utf8'})
   }
 
   private loadTemplate(templatePath: string) {
@@ -77,8 +124,8 @@ export class Builder {
     Object.entries(this.template).forEach(([relativeTemplatePath, template]) => {
       const targetDirname = path.resolve(componentOutputPath, path.dirname(relativeTemplatePath))
       mkdirp.sync(targetDirname)
-      const fileName = path.basename(relativeTemplatePath, 'ejs').replace(COMPONENT_NAME_PLACEHOLDER, componentName)
-      const content = ejs.render(template, {componentName, variables})
+      const fileName = path.basename(relativeTemplatePath, '.ejs').replace(COMPONENT_NAME_PLACEHOLDER, componentName)
+      const content = ejs.render(template, {componentName, variables, componentModuleName: COMPONENT_NAME_PLACEHOLDER})
       fs.writeFileSync(path.resolve(targetDirname, fileName), content, {encoding: 'utf8'})
     })
   }
